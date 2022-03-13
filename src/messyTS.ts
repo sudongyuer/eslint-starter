44dcbe0f9e3871bca645815c3dc3164288bf51e9
sudongyu <976499226@qq.com>
2022-03-13 11:27:42 +0800

    chore: add messy code

---
+++ src/messyTS.ts
@@ -0,0 +1,25 @@
+const x =       1
+
+const y = {
+  name:        "Jack",
+  age: 11,
+};
+
+console.log("z"    ,
+  x,
+  y
+
+);
+
+type Hello = {
+  name: string; age:number
+}
+
+const hello: Hello = {
+  name:
+
+    'zzz',
+  age: 1
+}
+
+console.log(hello);
