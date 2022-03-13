a0203a3d2af219c8f660985cfeafb2a403ffeee0
sudongyu <976499226@qq.com>
2022-03-13 11:27:31 +0800

    chore: add messy code

--- src/messyJS.js
+++ src/messyJS.js
@@ -1,4 +1,25 @@
-console.log('result'
-,x,
-  y
-)
+let y = {
+  name: "Jack",
+
+  age: 11,
+};
+
+console.log(y);
+
+// 原来的代码
+function foo() {
+  let isTrue;
+  return (
+    isTrue &&
+    [0, 1, 2].map(function (num) {
+      return num * 2;
+    })
+  );
+}
+foo();
+
+console.log({
+  name: "Jack",
+
+  age: 11,
+});
