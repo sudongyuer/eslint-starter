8c51b978875b880188148fb514a7e5212add28d7
sudongyu <976499226@qq.com>
2022-03-13 11:37:48 +0800

    chore: update

--- src/messyTS.ts
+++ src/messyTS.ts
@@ -1,25 +1,20 @@
-const x =       1
+const x = 1;
 
 const y = {
-  name:        "Jack",
+  name: "Jack",
   age: 11,
 };
 
-console.log("z"    ,
-  x,
-  y
-
-);
+console.log("z", x, y);
 
 type Hello = {
-  name: string; age:number
-}
+  name: string;
+  age: number;
+};
 
 const hello: Hello = {
-  name:
-
-    'zzz',
-  age: 1
-}
+  name: "zzz",
+  age: 1,
+};
 
 console.log(hello);
